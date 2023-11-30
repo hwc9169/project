736652afbcb0813d6d26e4f082314dd43d323847
hwc9169 <hwc9169@gmail.com>
2023-11-30 23:19:38 +0900

    Initial rocket library

---
+++ alpha/psql.sh
@@ -0,0 +1,1 @@
+docker run -it --rm postgres psql -h host.docker.internal -U alpha
--- alpha/src/bin/http-server.rs
+++ alpha/src/bin/http-server.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+async fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn launch() -> _ {
+    rocket::build().mount("/", routes![index])
 }
